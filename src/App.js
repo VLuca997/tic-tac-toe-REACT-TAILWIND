b2bda538c0dc9b5667ecccf9682bed55fbc521f5
Luca Vita <130995459+VLuca997@users.noreply.github.com>
2024-06-14 23:40:29 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p className='text-[50px] font-bold bg-yellow-400 text-red-500 text-center'>
+          LAYOUT CON TAILWINDCSS <br/>
+          <strong>FORZA ROMA</strong>
+          caioa
+        </p>
+    </div>
+  );
+}
+
+export default App;
